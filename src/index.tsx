0a22eb883b7c3a2ca04d97284d961f37edfe6f45
Isaac Adams <iadams@emoneyadvisor.com>
2020-03-31 01:09:42 -0400

    fixing up formatting

--- src/index.tsx
+++ src/index.tsx
@@ -48,16 +48,14 @@ function Resume(props) {
                                 right: ""
                             }
                         }} />
-                        <List 
-                            items={[
-                                "Helped usher in modern team practices such as pull requests, unit tests, and branch workflow",
-                                "Setup automated tasks for transpiling and minifying js and scss files using gulp",
-                                "Containerized and automated the build process using Docker to create a stable and flexible build environment",
-                                "Facilited the ease of releases by creating .nupkg through Jenkins multi branch pipeline and pushing to Octopus",
-                                "Built the data layer using a repostiory approach which inluded handling data in XML, JSON, and SQL",
-                                "Halved number of files in a project by introducing a simpler repository approach in the new .NET Core app",
-                            ]}
-                        />
+                        <List items={[
+                            "Helped usher in modern team practices such as pull requests, unit tests, and branch workflow",
+                            "Setup automated tasks for transpiling and minifying js and scss files using gulp",
+                            "Containerized and automated the build process using Docker to create a stable and flexible build environment",
+                            "Facilited the ease of releases by creating .nupkg through Jenkins multi branch pipeline and pushing to Octopus",
+                            "Built the data layer using a repostiory approach which inluded handling data in XML, JSON, and SQL",
+                            "Halved number of files in a project by introducing a simpler repository approach in the new .NET Core app",
+                        ]} />
                         <YSpacer />
                     </div> 
                     <div>
@@ -71,14 +69,12 @@ function Resume(props) {
                                 right: ""
                             }
                         }} />
-                        <List 
-                            items={[
-                                "Sped development by an estimated 20% after creating a T4 script that generated C# code based on SQL schema",
-                                "Developed a web and mobile application using Angular, C#, and SQL alongside a team of three other developers",
-                                "Wrote an API with ASP.Net Core and Entity Framework used to communicate between SQL and Angular",
-                                "Played a key role in the design of the entire application from the database to the user interface",
-                            ]}
-                        />
+                        <List items={[
+                            "Sped development by an estimated 20% after creating a T4 script that generated C# code based on SQL schema",
+                            "Developed a web and mobile application using Angular, C#, and SQL alongside a team of three other developers",
+                            "Wrote an API with ASP.Net Core and Entity Framework used to communicate between SQL and Angular",
+                            "Played a key role in the design of the entire application from the database to the user interface",
+                        ]} />
                         <YSpacer />
                     </div>
                 </ History>
@@ -94,12 +90,10 @@ function Resume(props) {
                                 right: ""
                             }
                         }} />
-                        <List 
-                            items={[
-                                "Built an API that communicates with a health insurance API and displays the coverage information to clients",
-                                "Designed the database in SQL, built the API using .NET Core, and used jQuery and bootstrap for the frontend",
-                            ]}
-                        />
+                        <List items={[
+                            "Built an API that communicates with a health insurance API and displays the coverage information to clients",
+                            "Designed the database in SQL, built the API using .NET Core, and used jQuery and bootstrap for the frontend",
+                        ]} />
                         <YSpacer />
                     </div> 
                     <div>
@@ -113,13 +107,11 @@ function Resume(props) {
                                 right: ""
                             }
                         }} />
-                        <List 
-                            items={[
-                                "Originally built the card game as a console application using pure C#",
-                                "Rebuilt the game using react so that actual pictures of cards could be used and the game could come to life",
-                                "Applied the use of specific data structures like stack and queue for the card game to work properly",
-                            ]}
-                        />
+                        <List items={[
+                            "Originally built the card game as a console application using pure C#",
+                            "Rebuilt the game using react so that actual pictures of cards could be used and the game could come to life",
+                            "Applied the use of specific data structures like stack and queue for the card game to work properly",
+                        ]} />
                         <YSpacer />
                     </div>
                 </ History>
