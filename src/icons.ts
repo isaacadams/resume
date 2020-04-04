ae4d183156921fbbe09b308ec7ed904954dc3744
Isaac Adams <isaac.d.adams@gmail.com>
2020-04-04 13:31:55 -0400

    :bulb: link to documentation on how to use font awesome library

--- src/icons.ts
+++ src/icons.ts
@@ -1,3 +1,5 @@
+// docs: https://github.com/FortAwesome/react-fontawesome/blob/master/README.md
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 //import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
