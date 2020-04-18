33ef4002aece2a007c76a0044d7f871c08d8e68a
Isaac Adams <isaac.d.adams@gmail.com>
2020-04-18 00:45:38 -0400

    :construction_worker: deploying octo release package to octopus

---
+++ octo-push.js
@@ -0,0 +1,27 @@
+let yargs = require('yargs'),
+    fs = require('fs'),
+    octo = require('@octopusdeploy/octopackjs');
+
+    let {path, hostname, apikey} = yargs
+    .demandOption('path', "path to the release package to push to the server")
+    .demandOption('hostname', "the hostname of the octopus server")
+    .demandOption('apikey', "a valid apiKey for the octopus server")
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+octo.push(path, {
+    host: hostname, 
+    apikey,
+    replace: true,
+    //spaceId: 'Spaces-1'
+}, function(err, result) {
+    
+    if(err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(result);
+    //console.log("Package Pushed:" + body.Title + " v"+ body.Version +" (" + fileSizeString(body.PackageSizeBytes) +"nytes)"); 
+});
