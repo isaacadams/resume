2299537f413b1f05f638759a0d1e03ed54c9fbb3
Isaac Adams <isaac.d.adams@gmail.com>
2020-08-13 14:57:14 -0400

    fixing print to pdf

--- print.js
+++ print.js
@@ -1,38 +1,15 @@
-var request = require("request");
-var fs = require('fs');
+// docs: https://github.com/segmentio/nightmare
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: true });
+let packageJson = require('./package.json');
 
-const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
- 
 nightmare
-    //.goto('https://duckduckgo.com') http://localhost:1234/
     .goto('http://localhost:1234/')
-    .evaluate(() => document.querySelector('.content'))
-    //.html('./dist/newtest.html', 'HTMLComplete')
-    .pdf('./dist/react.pdf', {
-      marginsType: 2,
-      pageSize: 'Letter'
+    .evaluate(() => document.querySelector('body')) //'.content'))
+    .pdf(`./dist/Resume v${packageJson.version}.pdf`, {
+      marginsType: 0,
+      pageSize: 'A3'
     })
     .end()
     .then(console.log)
-    .catch(error => {
-        console.error('Search failed: ', error)
-    });
-
-/* request({
-  //uri: "http://localhost:1234",
-  uri: "https://phantomjs.org/download.html"
-}, function(error, response, body) {
-  console.log(body);
-  fs.writeFileSync('./test.html', body);
-}).on('complete', () => {
-
-    console.log('done');
-    var html = fs.readFileSync('./test.html', 'utf8');
-    var options = { format: 'Letter' };
-
-    pdf.create(html, options).toFile('./test.pdf', function(err, res) {
-    if (err) return console.log(err);
-    console.log(res); // { filename: '/app/businesscard.pdf' }
-    });
-}); */
+    .catch(console.error);
