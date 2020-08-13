843b73f50b558fe25339b87f3884644cca7f6afe
Isaac Adams <isaac.d.adams@gmail.com>
2020-08-13 15:10:09 -0400

    removing old comments

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Resume from './Resume';
 
-/* import '../package/Resume.css';
-import Resume from '../package/Resume'; */
-
 ReactDOM.render(<Resume />, document.getElementById('resume'));
