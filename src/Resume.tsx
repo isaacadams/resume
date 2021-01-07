0f04b5daeb91504590ec289da768a5c80e6a9481
Isaac Adams <isaac.d.adams@gmail.com>
2021-01-07 10:48:27 -0500

    added GiftMe side project and updates from previous year at eMoney

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -16,19 +16,20 @@ export function Resume(props): JSX.Element {
                         },
                         bottom: {
                             left: "West Chester, PA", 
-                            right: <a href="https://github.com/isaacadams" ><FontAwesomeIcon icon={["fab", "github"]} /> isaacadams</a> //"(610) 742 6276"
+                            right: <a href="https://github.com/isaacadams" ><FontAwesomeIcon icon={["fab", "github"]} /> isaacadams</a>
+                            //right: "(610) 742 6276"
                         }
                     }} />
                     <YSpacer space="15px" />
                     <History title="Technical Skills">
                         <div className="row">
                             <div className="col mr-1">
-                                <Skill category="Languages" examples={["Javascript", "C#", "SQL", "TypeScript", "Python", "HTML", "LESS"]} />
+                                <Skill category="Languages" examples={["Javascript", "C#", "SQL", "Typescript", "Python", "HTML", "LESS"]} />
                                 <YSpacer />
                                 <Skill category="DevOps" examples={["Jenkins", "Docker", "Octopus", "Gulp", "IIS"]} />
                             </div> 
                             <div className="col">
-                                <Skill category="Technology" examples={[".NET Core", "React", "Node.js", "Angular", "EF Core", "jQuery", "NUnit", "API", "REST", "MVC"]} />
+                                <Skill category="Technology" examples={[".NET Core", "React", "GRPC", "SignalR", "Node.js", "Angular", "EF Core", "jQuery", "NUnit", "REST", "MVC"]} />
                                 <YSpacer />
                                 <Skill category="Other" examples={["Git", "Visual Studio", "JSON", "XML", "Jira"]} />
                             </div>
@@ -41,12 +42,12 @@ export function Resume(props): JSX.Element {
                             company={<i>eMoney Advisor</i>}
                             date="June 2018 - Present"
                             descriptions={[
+                                "Built a printing service which involved various technologies like GRPC, SignalR, IronPDF, and Encryption",
                                 "Helped usher in modern team practices such as pull requests, unit tests, and branch workflow",
+                                "Halved number of files in a project by introducing a simpler repository approach in the new .NET Core app",
                                 "Setup automated tasks for transpiling and minifying js and scss files using gulp",
                                 "Containerized and automated the build process using Docker to create a stable and flexible build environment",
                                 "Facilited the ease of releases by creating .nupkg through Jenkins multi branch pipeline and pushing to Octopus",
-                                "Built the data layer using a repostiory approach which inluded handling data in XML, JSON, and SQL",
-                                "Halved number of files in a project by introducing a simpler repository approach in the new .NET Core app"
                             ]}
                         />         
                         <YSpacer space="8px" />
@@ -64,6 +65,18 @@ export function Resume(props): JSX.Element {
                         <YSpacer space="10px" />
                     </ History>
                     <History title="Projects">
+                        <HistoryRecord 
+                            position={<b>GiftMe</b>}
+                            company={<i>Side Project</i>}
+                            date="December 2020 - Present"
+                            descriptions={[
+                                "A project where users can create wishlists and share them with friends and family",
+                                "Frontend is built using react and data is stored using Firebase Realtime Database",
+                                "Using parcel to bootstrap the application and typescript",
+                                "Currently working on feature that allows users create, edit, and manage groups for sharing wishlists"
+                            ]}
+                        />
+                        <YSpacer space="8px" />
                         <HistoryRecord 
                             position={<b>Health Coverage API</b>}
                             company={<i>Remote</i>}
